40650cb7c4bb1b08b9f61c62c378f7916824dfce
kuun <kuunwang@gmail.com>
2018-06-29 16:17:36 +0800

    save

--- src/main/java/org/webshark/view/DetailView.java
+++ src/main/java/org/webshark/view/DetailView.java
@@ -7,7 +7,12 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.util.Callback;
 import org.webshark.model.HeaderInfo;
 import org.webshark.viewmodel.DetailViewModel;
@@ -17,7 +22,9 @@ import java.util.ResourceBundle;
 
 public class DetailView implements FxmlView<DetailViewModel>, Initializable {
     @FXML
-    private TableView generalHeaderTable, requestHeaderTable, responseHeaderTable;
+    private ListView<HeaderInfo> generalHeaderList;
+    @FXML
+    private TableView requestHeaderTable, responseHeaderTable;
     @FXML
     private TableColumn<HeaderInfo, String> generalColFieldName,  generalColFieldValue;
     @FXML
@@ -45,6 +52,23 @@ public class DetailView implements FxmlView<DetailViewModel>, Initializable {
         }
     }
 
+    private class InfoChangeListener implements ChangeListener {
+        private ListView table;
+
+        public InfoChangeListener(ListView table) {
+            this.table = table;
+        }
+
+        @Override
+        public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+            var height = table.fixedCellSizeProperty().get() * table.getItems().size() + 20;
+            if (height == 0) {
+                height = 24;
+            }
+            table.setPrefHeight(height);
+        }
+    }
+
     private static class CellFactory implements Callback<TableColumn<HeaderInfo, String>, TableCell<HeaderInfo, String>> {
         @Override
         public TableCell<HeaderInfo, String> call(TableColumn<HeaderInfo, String> param) {
@@ -58,6 +82,31 @@ public class DetailView implements FxmlView<DetailViewModel>, Initializable {
         }
     }
 
+    private static class HeaderInfoCell extends ListCell<HeaderInfo> {
+        static final Font boldFont = Font.font(Font.getDefault().getFamily(), FontWeight.BOLD, Font.getDefault().getSize());
+        @Override
+        protected void updateItem(HeaderInfo item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (empty || item == null) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                var pane = new StackPane();
+                TextFlow textFlow = new TextFlow();
+                textFlow.setPrefWidth(Region.USE_COMPUTED_SIZE);
+                var nameText = new Label(item.getFieldName() + " : ");
+                nameText.setFont(boldFont);
+                var valueText = new Label(item.getFieldValue());
+                valueText.setEllipsisString("...");
+                valueText.setPrefWidth(Region.USE_PREF_SIZE);
+                textFlow.getChildren().addAll(nameText, valueText);
+                // pane.getChildren().add(textFlow);
+                setGraphic(textFlow);
+            }
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Callback<TableColumn.CellDataFeatures<HeaderInfo, String>, ObservableValue<String>> fieldNameCellFactory =
@@ -66,10 +115,10 @@ public class DetailView implements FxmlView<DetailViewModel>, Initializable {
             (info) -> info.getValue().fieldValueProperty();
         var cellFactory = new CellFactory();
 
-        generalColFieldName.setCellValueFactory(fieldNameCellFactory);
-        generalColFieldName.setCellFactory(cellFactory);
-        generalColFieldValue.setCellValueFactory(filedValueCellFactory);
-        generalColFieldValue.setCellFactory(cellFactory);
+//        generalColFieldName.setCellValueFactory(fieldNameCellFactory);
+//        generalColFieldName.setCellFactory(cellFactory);
+//        generalColFieldValue.setCellValueFactory(filedValueCellFactory);
+//        generalColFieldValue.setCellFactory(cellFactory);
         requestColFieldName.setCellValueFactory(fieldNameCellFactory);
         requestColFieldName.setCellFactory(cellFactory);
         requestColFieldValue.setCellValueFactory(filedValueCellFactory);
@@ -79,8 +128,10 @@ public class DetailView implements FxmlView<DetailViewModel>, Initializable {
         responseColFieldValue.setCellValueFactory(filedValueCellFactory);
         responseColFieldValue.setCellFactory(cellFactory);
 
-        generalHeaderTable.itemsProperty().bind(viewModel.generalHeadersProperty());
-        generalHeaderTable.itemsProperty().addListener(new DetailChangeListener(generalHeaderTable));
+        generalHeaderList.itemsProperty().bind(viewModel.generalHeadersProperty());
+        generalHeaderList.setCellFactory((list) -> new HeaderInfoCell());
+        generalHeaderList.itemsProperty().addListener(new InfoChangeListener(generalHeaderList));
+//        generalHeaderList.itemsProperty().addListener(new DetailChangeListener(generalHeaderList));
         requestHeaderTable.itemsProperty().bind(viewModel.requestHeadersProperty());
         requestHeaderTable.itemsProperty().addListener(new DetailChangeListener(requestHeaderTable));
         responseHeaderTable.itemsProperty().bind(viewModel.responseHeadersProperty());
