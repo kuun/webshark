d4a2bedaa44641d2cd2e44a6322739a8a0e58b4f
wangkun <wangkun@goldensecurity.com.cn>
2018-10-11 12:19:23 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  /* your base configuration of choice */
+  extends: [
+    'eslint:recommended',
+    "plugin:react/recommended"
+  ],
+
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  globals: {
+    __static: true
+  }
+}
