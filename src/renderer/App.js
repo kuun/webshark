69df4ac19ca5a19164c85925235a4cccdf058e98
kuun <kuunwang@gmail.com>
2019-09-04 17:56:00 +0800

    replace react-grid-layout with react-split-pane

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
-import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 import './App.css';
 import './components/ProxyStartForm'
